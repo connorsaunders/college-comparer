397d2da4077e00079cd1b2f0a7a6a48162f566d0
Connor Saunders <connorsaunders18@gmail.com>
2023-05-06 14:06:16 -0400

    del button and css

--- src/components/InputTable.js
+++ src/components/InputTable.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
 import SearchItems from './SearchItems';
-
-//Table for school codes and query for tuition
 import { School_2_Codes_Dict } from "../data/School_to_Code";
 import { tuitionQuery } from '../apiqueries/TuitionAPICall';
-
-//Table for major codes and query for salary
-import { salaryQuery } from '../apiqueries/SalaryAPICall';
-import { Major_2_CIPCode } from '../data/Major_to_CIPCode';
 
 function InputTable(props) {
   const { input1, setInput1, input2, setInput2, tableData, tuitionState, setTuitionState, setTableData } = props;
@@ -22,8 +16,13 @@ function InputTable(props) {
 
     const updatedTableData = [...tableData, { input1, input2, tuitionState, tuition1, tuition2 }];
     setTableData(updatedTableData);
-
   };
+
+  const handleDelete = (index) => {
+    const updatedTableData = [...tableData];
+    updatedTableData.splice(index, 1);
+    setTableData(updatedTableData);
+  }
 
   return (
     <div>
@@ -31,24 +30,18 @@ function InputTable(props) {
         <h1 className="my-header">College Comparison Tool</h1>
         <h2 className="my-subheader">Find and Compare the ROI in your Education</h2>
         <SearchItems input1={input1} setInput1={setInput1} input2={input2} setInput2={setInput2} handleSubmit={handleSubmit} tuitionState={tuitionState} setTuitionState={setTuitionState} />
-        <table>
+        <table className="my-table">
           <thead>
             <tr>
               <th>College</th>
               <th>Major</th>
               <th>In/Out State</th>
-              <th colspan="2">Cost of attendance</th>
-              <th colspan="3">Median Salary</th>
-            </tr>
-            <tr>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th>1 yr.</th>
-              <th>4 yrs.</th>
-              <th>6 yrs.</th>
-              <th>3 yrs.</th>
-              <th>1 yr.</th>
+              <th>Cost of attendance (1 yr.)</th>
+              <th>Cost of attendance (4 yrs.)</th>
+              <th>Median Salary (6 yrs.)</th>
+              <th>Median Salary (3 yrs.)</th>
+              <th>Median Salary (1 yr.)</th>
+              <th></th> {/* Add an additional column for the delete button */}
             </tr>
           </thead>
           <tbody>
@@ -62,6 +55,11 @@ function InputTable(props) {
                 <td>{"$ _____"}</td>
                 <td>{"$ _____"}</td>
                 <td>{"$ _____"}</td>
+                <td>
+                  <button onClick={() => handleDelete(index)} style={{ border: 'none', backgroundColor: 'red', color: 'white', width: '30px', height: '30px', borderRadius: '5px', fontSize: '20px', transition: 'background-color 0.5s ease-in-out', marginTop: '5px'}}>
+                    -
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -78,6 +76,6 @@ function InputTable(props) {
       </div>
     </div>
   );
-}
+}  
 
 export default InputTable;
