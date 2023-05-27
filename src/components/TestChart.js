92d7f534a4708a78e00c5aab45582e6dc935d0d5
Connor Saunders <connorsaunders18@gmail.com>
2023-05-27 16:50:03 -0400

    Updates to Chart

--- src/components/TestChart.js
+++ src/components/TestChart.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//                                  Imports
+////////////////////////////////////////////////////////////////////////////////////////
+
 import React from 'react';
 import {
   Chart as ChartJS,
@@ -21,8 +25,13 @@ ChartJS.register(
   Legend
 );
 
+////////////////////////////////////////////////////////////////////////////////////////
+//                                  Table Options
+////////////////////////////////////////////////////////////////////////////////////////
+
 export const options = {
   responsive: true,
+  
   plugins: {
     legend: {
       position: 'top',
@@ -40,20 +49,14 @@ export const options = {
               lineWidth: dataset.borderWidth,
               strokeStyle: dataset.borderColor,
               pointStyle: dataset.pointStyle,
-
-              // Below is extra data used for the onClick event
               datasetIndex: i
             };
           });
-
-          // Pair each two legends into a single item.
           let pairLegends = [];
           for (let i = 0; i < legends.length; i += 2) {
             pairLegends.push({
-              // Combine text from both legends
               text: `${legends[i].text}`,
 
-              // Choose one color or mix as you prefer
               fillStyle: legends[i].fillStyle,
 
               hidden: legends[i].hidden,
@@ -65,11 +68,9 @@ export const options = {
               strokeStyle: legends[i].strokeStyle,
               pointStyle: legends[i].pointStyle,
 
-              // Keep the dataset indices
               datasetIndices: [legends[i].datasetIndex, legends[i+1].datasetIndex]
             });
           }
-
           return pairLegends.map((legend) => {
             return {
               ...legend,
@@ -79,7 +80,6 @@ export const options = {
         }
       },
       onClick: (evt, item, legend) => {
-        // Toggle both datasets when the legend item is clicked
         let ci = legend.chart;
         item.datasetIndices.forEach((datasetIndex) => {
           let meta = ci.getDatasetMeta(datasetIndex);
@@ -106,6 +106,7 @@ export const options = {
         text: 'Median Salary:',
       },
       min: 0,
+    
       stepSize: 20000,
       ticks: {
         callback: function(value, index, values) {
@@ -114,92 +115,111 @@ export const options = {
       },
     },
   },
+      // https://www.chartjs.org/docs/latest/configuration/animations.html
+      animation: {
+        easing: 'easeOutBounce',
+      }
+      
+      
 };
 
-function generateProjection(y4) {
-  const n = 16; // Number of years starting from 4 years post-graduation
-  const r = 0.0766; // Annual growth rate
+////////////////////////////////////////////////////////////////////////////////////////
+//                              Generating Projection
+////////////////////////////////////////////////////////////////////////////////////////
 
-  let data = [];
+function generateProjection(y1, y4) {
+  const years = [1, 4, 10, 15, 20, 25, 30]; // New years array
+  const r = 0.03;
+
+  let data = [y1, y4]; // Start with y1 and y4
+
   let currentYearSalary = y4;
 
-  for (let x = 4; x <= n + 3; x += 1) {
+  for (let x = 2; x < years.length; x++) {
+    // Compute the number of years to multiply the current salary by.
+    const numYears = years[x] - years[x - 1];
+    currentYearSalary *= Math.pow(1 + r, numYears);
     data.push(currentYearSalary);
-    currentYearSalary *= (1 + r);
   }
 
   return data;
 }
-
-
+////////////////////////////////////////////////////////////////////////////////////////
+//                                  Table Data
+////////////////////////////////////////////////////////////////////////////////////////
 
 export function TestChart({ tableData }) {
-  const labels = ['1 Year', '4 Years', '7 Years', '10 Years', '13 Years', '16 Years','19 Years'];
+  const labels = ['1 Year', '4 Years', '10 Years', '15 Years', '20 Years', '25 Years', '30 Years'];
 
   const colors = [
     'rgb(255, 99, 132)', // Red
     'rgb(54, 162, 235)', // Blue
     'rgb(255, 206, 86)', // Yellow
     'rgb(75, 192, 192)', // Green
-  ];  
+  ];
 
   const data = {
     labels,
-    datasets: []
+    datasets: [],
   };
-tableData.forEach((row, index) => {
-  const color = colors[index % colors.length];
 
-  // if neither year1_salary nor year4_salary is present, just continue to the next row
-  if ((row.year1_salary === null || row.year1_salary === undefined) && (row.year4_salary === null || row.year4_salary === undefined)) {
-    return;
-  }
+////////////////////////////////////////////////////////////////////////////////////////
+//                                  Implement Each Row
+////////////////////////////////////////////////////////////////////////////////////////
 
-  let year1_salary = row.year1_salary;
-  let year4_salary = row.year4_salary;
+  tableData.forEach((row, index) => {
+    const color = colors[index % colors.length];
 
-  // if year1_salary is not present, estimate it as year4_salary divided by a 9% increase over 3 years
-  if (row.year1_salary === null || row.year1_salary === undefined) {
-    year1_salary = year4_salary / Math.pow(1.0776, 3);
-  }
+    // if neither year1_salary nor year4_salary is present, just continue to the next row
+    if (
+      (row.year1_salary === null || row.year1_salary === undefined) &&
+      (row.year4_salary === null || row.year4_salary === undefined)
+    ) {
+      return;
+    }
 
-  // if year4_salary is not present, estimate it as year1_salary increased by 9% over 3 years
-  if (row.year4_salary === null || row.year4_salary === undefined) {
-    year4_salary = year1_salary * Math.pow(1.0776, 3);
-  }
+    let year1_salary = row.year1_salary;
+    let year4_salary = row.year4_salary;
 
-  const logData = generateProjection(year4_salary);
+    // if year1_salary is not present, estimate it as year4_salary divided by a 9% increase over 3 years
+    if (row.year1_salary === null || row.year1_salary === undefined) {
+      year1_salary = year4_salary / Math.pow(1 + 0.03, 3);
+    }
 
-  // Calculate the number of missing years to shift the line
-  let missingYears = 0;
-  if (row.year1_salary === null || row.year1_salary === undefined) {
-    missingYears = 2; // If year1 is missing, we have 3 missing years
-  }
+    // if year4_salary is not present, estimate it as year1_salary increased by 9% over 3 years
+    if (row.year4_salary === null || row.year4_salary === undefined) {
+      year4_salary = year1_salary * Math.pow(1 + 0.03, 3);
+    }
 
-  // solid line portion (from 1 to 4)
-  data.datasets.push({
-    label: `${row.input1}, ${row.input2}`,
-    data: [year1_salary, year4_salary],
-    borderColor: color,
-    backgroundColor: `${color}`,
-    fill: false,
-    order: index,
+    const logData = generateProjection(year1_salary, year4_salary);
+
+    // solid line portion (from 1 to 4)
+    data.datasets.push({
+      label: `${row.input1}, ${row.input2}`,
+      data: [year1_salary, year4_salary],
+      borderColor: color,
+      backgroundColor: `${color}`,
+      fill: false,
+      order: index,
+    });
+
+    // dotted line portion
+    data.datasets.push({
+      label: `${row.input1}, ${row.input2}` + ' 3% Annual Increase Projection',
+      data: [...logData.slice(0)],
+      borderColor: color,
+      backgroundColor: `${color}`,
+      fill: false,
+      borderDash: [5, 10],
+      order: index,
+      pointHitRadius: 0,
+    });
   });
 
-  // dotted line portion (logarithmic projection starting from 4)
-  data.datasets.push({
-    label: `${row.input1}, ${row.input2}`+' Logarithmic Projection',
-    data: [null,...logData.slice(0)],
-    borderColor: color,
-    backgroundColor: `${color}`,
-    fill: false,
-    borderDash: [5, 5],
-    order: index,
-    pointHitRadius: 0,  // Add this line
-  });
-});
+////////////////////////////////////////////////////////////////////////////////////////
+//                                  Return the Chart
+////////////////////////////////////////////////////////////////////////////////////////
 
-    
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ height: '500px', width: '100%', maxWidth: '1100px', padding: '0 20px', marginBottom: '10px', marginTop: '10px' }}>
@@ -208,4 +228,3 @@ tableData.forEach((row, index) => {
     </div>
   );
 }
-
