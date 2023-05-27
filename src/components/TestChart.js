2be2578924df2b18fe69835a145ca787a048aa9a
Connor Saunders <connorsaunders18@gmail.com>
2023-05-27 09:52:46 -0400

    Updated Chart

--- src/components/TestChart.js
+++ src/components/TestChart.js
@@ -131,90 +131,81 @@ function generateProjection(y4) {
   return data;
 }
 
+
+
 export function TestChart({ tableData }) {
-  const labels = ['1 Year', '4 Years', '7 Years', '10 Years', '13 Years', '16 Years', '19 Years'];
+  const labels = ['1 Year', '4 Years', '7 Years', '10 Years', '13 Years', '16 Years','19 Years'];
 
   const colors = [
     'rgb(255, 99, 132)', // Red
     'rgb(54, 162, 235)', // Blue
     'rgb(255, 206, 86)', // Yellow
     'rgb(75, 192, 192)', // Green
-  ];
+  ];  
 
   const data = {
     labels,
-    datasets: [],
+    datasets: []
   };
+tableData.forEach((row, index) => {
+  const color = colors[index % colors.length];
 
-  tableData.forEach((row, index) => {
-    const color = colors[index % colors.length];
+  // if neither year1_salary nor year4_salary is present, just continue to the next row
+  if ((row.year1_salary === null || row.year1_salary === undefined) && (row.year4_salary === null || row.year4_salary === undefined)) {
+    return;
+  }
 
-    // if neither year1_salary nor year4_salary is present, just continue to the next row
-    if ((row.year1_salary === null || row.year1_salary === undefined) && (row.year4_salary === null || row.year4_salary === undefined)) {
-      return;
-    }
+  let year1_salary = row.year1_salary;
+  let year4_salary = row.year4_salary;
 
-    let year1_salary = row.year1_salary;
-    let year4_salary = row.year4_salary;
+  // if year1_salary is not present, estimate it as year4_salary divided by a 9% increase over 3 years
+  if (row.year1_salary === null || row.year1_salary === undefined) {
+    year1_salary = year4_salary / Math.pow(1.0776, 3);
+  }
 
-    // if year1_salary is not present, estimate it as year4_salary divided by a 9% increase over 3 years
-    if (row.year1_salary === null || row.year1_salary === undefined) {
-      year1_salary = year4_salary / Math.pow(1.0776, 3);
-    }
+  // if year4_salary is not present, estimate it as year1_salary increased by 9% over 3 years
+  if (row.year4_salary === null || row.year4_salary === undefined) {
+    year4_salary = year1_salary * Math.pow(1.0776, 3);
+  }
 
-    // if year4_salary is not present, estimate it as year1_salary increased by 9% over 3 years
-    if (row.year4_salary === null || row.year4_salary === undefined) {
-      year4_salary = year1_salary * Math.pow(1.0776, 3);
-    }
+  const logData = generateProjection(year4_salary);
 
-    const logData = generateProjection(year4_salary);
+  // Calculate the number of missing years to shift the line
+  let missingYears = 0;
+  if (row.year1_salary === null || row.year1_salary === undefined) {
+    missingYears = 2; // If year1 is missing, we have 3 missing years
+  }
 
-    // Calculate the number of missing years to shift the line
-    let missingYears = 0;
-    if (row.year1_salary === null || row.year1_salary === undefined) {
-      missingYears = 2; // If year1 is missing, we have 3 missing years
-    }
-
-    // solid line portion (from 1 to 4)
-    data.datasets.push({
-      label: `${row.input1}, ${row.input2}`,
-      data: [year1_salary, year4_salary],
-      borderColor: color,
-      backgroundColor: `${color}`,
-      fill: false,
-      order: index,
-    });
-
-    // dotted line portion (projection starting from 4)
-    data.datasets.push({
-      label: `${row.input1}, ${row.input2}` + ' Projection',
-      data: [null, ...logData.slice(0)],
-      borderColor: color,
-      backgroundColor: `${color}`,
-      fill: false,
-      borderDash: [5, 5],
-      order: index,
-      pointHitRadius: 0,
-    });
+  // solid line portion (from 1 to 4)
+  data.datasets.push({
+    label: `${row.input1}, ${row.input2}`,
+    data: [year1_salary, year4_salary],
+    borderColor: color,
+    backgroundColor: `${color}`,
+    fill: false,
+    order: index,
   });
 
-  const maxSalary = Math.max(...data.datasets.flatMap((dataset) => dataset.data));
-  options.scales.y.max = Math.ceil(maxSalary / 100000) * 100000; // Set the maximum y-axis value
+  // dotted line portion (logarithmic projection starting from 4)
+  data.datasets.push({
+    label: `${row.input1}, ${row.input2}`+' Logarithmic Projection',
+    data: [null,...logData.slice(0)],
+    borderColor: color,
+    backgroundColor: `${color}`,
+    fill: false,
+    borderDash: [5, 5],
+    order: index,
+    pointHitRadius: 0,  // Add this line
+  });
+});
 
+    
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div
-        style={{
-          height: '500px',
-          width: '100%',
-          maxWidth: '1100px',
-          padding: '0 20px',
-          marginBottom: '10px',
-          marginTop: '10px',
-        }}
-      >
+      <div style={{ height: '500px', width: '100%', maxWidth: '1100px', padding: '0 20px', marginBottom: '10px', marginTop: '10px' }}>
         <Line data={data} options={options} />
       </div>
     </div>
   );
 }
+
