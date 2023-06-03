547d6da542b0380dbefa930fac82ec0bf8d7b9f3
Connor Saunders <connorsaunders18@gmail.com>
2023-06-03 10:09:04 -0400

    Increased header size

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import '../css/Header.css';
 function Header() {
     return (
         <div className="header">
-            <br />
-            <h1>
+            <br/>
+            <h1 style={{ fontSize: '50px' }}>
                 <span className="highlight">College Cost </span> 
                 <span className="blue">Comparison Tool</span>
             </h1>
